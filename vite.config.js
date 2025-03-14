49f985ccca4ff0c92b491fac213c61d78d90dcfc
Daniel de Santana Alves <dalves@desenbahia.ba.gov.br>
2025-03-14 09:21:13 -0300

    Corrigindo deploy no Railway

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,16 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  // Carregar variáveis do .env
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    server: {
-      host: true, // Permite acesso externo
-      port: env.VITE_PORT || 3000, // Garante que use a porta correta
-      strictPort: true,
-    },
-    preview: {
-      host: true,
-      port: env.VITE_PORT || 3000,
-      strictPort: true,
-    },
-    define: {
-      "process.env": env, // Define as variáveis para o ambiente do Vite
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000, // Usa a porta que o Railway definir
+    strictPort: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+    strictPort: true,
+  },
 });
